bb4e33cf4c125601e36a161e4495b0c218b84923
segundocristian219-oss <segundocristian219@gmail.com>
2026-02-04 12:20:05 -0600

    Crear unbanuser.js

---
+++ plugins/owner/unbanuser.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+   let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    else who = m.chat
+    let user = global.db.data.users[who]
+    if (!who) return m.reply(`🚩 Etiqueta a un usuario.`)
+    let users = global.db.data.users
+    users[who].banned = false
+    conn.reply(m.chat, `🚩 @${who.split`@`[0]} ha sido desbaneado con exito, ahora podrá volver a usar mis comandos.`, m, { mentions: [who] })
+}
+handler.help = ['unban *@user*']
+handler.tags = ['owner']
+handler.command = /^unban$/i
+handler.owner = true
+
+export default handler
