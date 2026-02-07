bf2b8ddb3c1cb5c81869420f34e51156eda7f6ec
segundocristian219-oss <segundocristian219@gmail.com>
2026-02-07 00:47:06 -0600

    Actualizar restart.js

--- plugins/owner/restart.js
+++ plugins/owner/restart.js
@@ -11,7 +11,7 @@ const handler = async (msg, { conn }) => {
 
   // Mensaje de aviso
   await conn.sendMessage(chatId, {
-    text: "🔄 *𝐅𝐎𝐗 𝐁𝐀𝐋𝐀 𝐁𝐎𝐓 se reiniciará en unos segundos...*"
+    text: "🔄 *𝐃𝐗𝐍𝐍𝐘.𝐁𝐎𝐓 se reiniciará en unos segundos...*"
   }, { quoted: msg });
 
   // Guardar chat para notificar luego
