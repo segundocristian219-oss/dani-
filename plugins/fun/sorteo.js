48b1e200582dca1e88d4434ff76fc19333f4c082
segundocristian219-oss <segundocristian219@gmail.com>
2026-03-26 10:24:21 -0600

    Actualizar sorteo.js

--- plugins/fun/sorteo.js
+++ plugins/fun/sorteo.js
@@ -4,21 +4,23 @@ let handler = async (m, { conn, participants }) => {
   if (!m.isGroup)
     return conn.reply(m.chat, 'Este comando solo funciona en grupos.', m);
 
-  const normalize = (jid = '') =>
-    jid.split(':')[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net';
+  const clean = (jid = '') =>
+    jid.split(':')[0].replace(/[^0-9]/g, '');
 
-  const botId = normalize(conn.user.id || conn.user.jid || '');
-  const senderId = normalize(m.sender);
+  const botNum = clean(conn.user.id || conn.user.jid || '');
+  const senderNum = clean(m.sender);
 
   let candidates = participants
-    .map(p => normalize(p.id))
-    .filter(id => id !== botId && id !== senderId);
+    .filter(p => {
+      const num = clean(p.id);
+      return num !== botNum && num !== senderNum;
+    })
+    .map(p => p.id);
 
   if (!candidates.length)
     return conn.reply(m.chat, 'No hay candidatos válidos para elegir.', m);
 
-  const index = crypto.randomInt(0, candidates.length);
-  const chosen = candidates[index];
+  const chosen = candidates[crypto.randomInt(0, candidates.length)];
 
   const text = `🎯 El elegido es: @${chosen.split('@')[0]}`;
 
@@ -31,7 +33,7 @@ let handler = async (m, { conn, participants }) => {
 
 handler.help = ["𝖱𝗎𝗅𝖾𝗍𝖺"];
 handler.tags = ["𝖦𝖱𝖴𝖯𝖮𝖲"];
-handler.command = ['sorteo'];
+handler.command = ['ruleta'];
 handler.group = true;
 
 export default handler;
